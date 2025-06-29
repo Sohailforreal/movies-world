2b90d73465ede049f5a954635ce918c99d629c0b
Sohailforreal <smsohail027@gmail.com>
2025-06-29 09:26:24 +0530

    v.Alert

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,10 @@ import {getTrendingMovies} from './appwrite.js'
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
+if (!API_KEY) {
+  alert("Missing TMDB API Key");
+}
+
 const API_OPTIONS = {
   method: 'GET',
   headers: {

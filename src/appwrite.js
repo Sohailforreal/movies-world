67894fcd40230bdb07a57f3dd1cbfad398c7a649
Sohailforreal <smsohail027@gmail.com>
2025-06-29 08:56:08 +0530

    v.Mo

--- src/appwrite.js
+++ src/appwrite.js
@@ -2,12 +2,13 @@ import { Client, Databases, Query, ID} from 'appwrite';
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+const ENDPOINT_ID = import.meta.env.VITE_APPWRITE_ENDPOINT_ID;
 
 
 
 
 const client = new Client()
-.setEndpoint('https://fra.cloud.appwrite.io/v1')
+.setEndpoint(ENDPOINT_ID)
 .setProject(PROJECT_ID)
 
 const database = new Databases(client);
